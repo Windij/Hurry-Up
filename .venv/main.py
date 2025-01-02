cdbfbceb20574019326f46919bf02abeb53a5420
WINDIJ <max.spivak@mail.ru>
2025-01-02 19:17:35 +0500

    Добавил Player, но без проверки пересечения с Wall

--- .venv/main.py
+++ .venv/main.py
@@ -1,6 +1,8 @@
 import pygame
 import os
 
+from pygame.examples.sprite_texture import sprite
+
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 BOARD_WIDTH = 9
@@ -10,7 +12,7 @@ GRAY = (200, 200, 200)
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 GRID_LINE_COLOR = BLACK
-LEVEL_FILE = '../level.txt'
+LEVEL_FILE = 'level.txt'
 
 class Base(pygame.sprite.Sprite):
     def __init__(self, x, y, color):
@@ -29,6 +31,15 @@ class Wall(Base):
     def __init__(self, x, y):
         super().__init__(x, y, RED)
 
+class Player(Base):
+    def __init__(self, x, y):
+        super().__init__(x, y, GRAY)
+    def move(self, dx, dy):
+        pass
+    def is_collide(self, all_tiles):
+        return pygame.sprite.spritecollideany(self,all_tiles)
+
+
 def load_level(filename):
     level_data = []
     try:
@@ -42,12 +53,16 @@ def load_level(filename):
 
 def create_level(level_data):
     all_tiles = pygame.sprite.Group()
+    player = pygame.sprite.Group()
     for y, row in enumerate(level_data):
         for x, tile_type in enumerate(row):
             if tile_type == '#':
                 tile = Wall(x, y)
                 all_tiles.add(tile)
-    return all_tiles
+            elif tile_type == 'P':
+                p1 = Player(x,y)
+                player.add(p1)
+    return all_tiles, player, p1
 
 class Board:
     def __init__(self, width, height):
@@ -64,17 +79,17 @@ class Board:
         self.top = top
         self.cell_size = cell_size
 
-    def render(self, screen):
-        for y in range(self.height):
-            for x in range(self.width):
-                pygame.draw.rect(screen, 'white', (
-                    x * self.cell_size + self.left,
-                    y * self.cell_size + self.top, self.cell_size,
-                    self.cell_size), width=1)
-                pygame.draw.rect(screen, 'grey', (
-                    x * self.cell_size + self.left + 1,
-                    y * self.cell_size + self.top + 1, self.cell_size - 2,
-                    self.cell_size - 2))
+    # def render(self, screen):
+    #     for y in range(self.height):
+    #         for x in range(self.width):
+    #             pygame.draw.rect(screen, 'white', (
+    #                 x * self.cell_size + self.left,
+    #                 y * self.cell_size + self.top, self.cell_size,
+    #                 self.cell_size), width=1)
+    #             pygame.draw.rect(screen, 'grey', (
+    #                 x * self.cell_size + self.left + 1,
+    #                 y * self.cell_size + self.top + 1, self.cell_size - 2,
+    #                 self.cell_size - 2))
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
@@ -94,8 +109,11 @@ class Board:
 
     def load_level(self, filename):
         level_data = load_level(filename)
-        self.all_tiles = create_level(level_data)
+        self.all_tiles, self.player, self.p1 = create_level(level_data)
         for tile in self.all_tiles:
+            tile.rect.x += self.left
+            tile.rect.y += self.top
+        for tile in self.player:
             tile.rect.x += self.left
             tile.rect.y += self.top
 
@@ -104,7 +122,8 @@ class Board:
             tile.move(dx, dy)
 
     def draw_level(self, screen):
-       self.all_tiles.draw(screen)
+        self.all_tiles.draw(screen)
+        self.player.draw(screen)
 
 
 def main():
@@ -124,20 +143,20 @@ def main():
             if event.type == pygame.KEYDOWN:
               dx = 0
               dy = 0
-              if event.key == pygame.K_s:
-                  dx = -1
-              if event.key == pygame.K_e:
-                  dy = -1
               if event.key == pygame.K_d:
+                  dx = -1
+              if event.key == pygame.K_s:
+                  dy = -1
+              if event.key == pygame.K_w:
                   dy = 1
-              if event.key == pygame.K_f:
+              if event.key == pygame.K_a:
                   dx = 1
               board.move_level(dx, dy)
 
 
         screen.fill(BLACK)
         board.draw_level(screen)
-        board.render(screen)
+        # board.render(screen)
         pygame.display.flip()
 
     pygame.quit()
@@ -145,7 +164,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
